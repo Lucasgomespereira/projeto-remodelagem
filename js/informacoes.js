620868ec269b8df007592369cd05955589ada172
Lucas Gomes <201810451@aluno.fjn.edu.br>
2020-06-15 19:58:52 -0300

    Implementação da ação ver mais, para deixar o ambiente mobile mais limpo

---
+++ js/informacoes.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#linkVerMais1").click(function () {
+        $("#verMais1").removeClass('verMais');
+        $("#linkVerMais1").addClass('verMais');
+    });
+    $("#linkVerMais2").click(function () {
+        $("#verMais2").removeClass('verMais');
+        $("#linkVerMais2").addClass('verMais');
+    });
+    $("#linkVerMais3").click(function () {
+        $("#verMais3").removeClass('verMais');
+        $("#linkVerMais3").addClass('verMais');
+    });
+    $("#linkVerMais4").click(function () {
+        $("#verMais4").removeClass('verMais');
+        $("#linkVerMais4").addClass('verMais');
+    });
+
+});
